724eae5eb065589c796acef1f97fb106fbbb9290
NakotaC <nakotac@iastate.edu>
2024-11-18 00:49:30 -0600

    I think bubblesort works now. Need to test on a lab computer due to the simulation timing out. Added Nops, so code needs simplified.

--- cpre381-toolflow/proj/mips/Proj2Tests/Proj2_bubblesort.s
+++ cpre381-toolflow/proj/mips/Proj2Tests/Proj2_bubblesort.s
@@ -21,45 +21,79 @@ main:
     add $t0, $0, $0          # i = 0
 
 outer_loop:
-    add $t1, $0, $0          # j = 0
+    add $t2, $0, $0          # j = 0
+    nop
     sub $t2, $s1, $t0        # t2 = size - i
     nop                      # Avoid hazard with $t2
+    nop
+    nop
     addi $t2, $t2, -1        # t2 = size - i - 1
 
 inner_loop:
     # Calculate array index - Example 1 of optimized scheduling:
     # No NOPs needed between these as sll uses $t1 and add uses $t3
     sll $t3, $t1, 2          # t3 = j * 4
+    nop
+    nop
+    nop
     add $t3, $s0, $t3        # t3 = base + j*4
+    nop
+    nop
+    nop
     
     # Load elements - Example 2 of optimized scheduling:
     # Only one NOP needed between loads as they use different addresses
     lw  $t4, 0($t3)          # t4 = array[j]
-    nop                      # Single NOP sufficient
     lw  $t5, 4($t3)          # t5 = array[j+1]
     
     # Compare elements - Example 3 of optimized scheduling:
     # No NOPs needed between slt and beq as they're independent
     nop                      # Wait for $t4 and $t5
+    nop
+    nop
     slt $t6, $t5, $t4        # t6 = 1 if array[j+1] < array[j]
+    nop
+    nop
+    nop
     beq $t6, $0, no_swap     # Skip swap if t6 = 0
     nop                      # Branch delay slot
+    nop
+    nop
     
     # Swap elements
     sw  $t5, 0($t3)          # array[j] = t5
+    nop
     sw  $t4, 4($t3)          # array[j+1] = t4
 
 no_swap:
+    nop
+    nop
+    nop
+    nop
     addi $t1, $t1, 1         # j++
     nop                      # Avoid hazard with next slt
+    nop
+    nop
     slt $t6, $t1, $t2        # t6 = 1 if j < size-i-1
+    nop
+    nop
+    nop
     bne $t6, $0, inner_loop  # if j < size-i-1, continue inner loop
     nop                      # Branch delay slot
+    nop
+    nop
 
     addi $t0, $t0, 1         # i++
     nop                      # Avoid hazard with next slt
+    nop
+    nop
     slt $t6, $t0, $s1        # t6 = 1 if i < size
+    nop
+    nop
+    nop
     bne $t6, $0, outer_loop  # if i < size, continue outer loop
     nop                      # Branch delay slot
+    nop
+    nop
 
     halt
